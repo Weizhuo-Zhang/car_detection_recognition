57f6ec23650800ce0578967f539f0a96939295ba
Weizhuo Zhang <wzhuo.zhang@gmail.com>
2019-09-21 20:51:13 +1000

    replace yolov3 model file name

--- zwz_folder/tf_example/lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ zwz_folder/tf_example/lite/examples/object_detection/android/app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -52,8 +52,8 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
   // Configuration values for the prepackaged SSD model.
   private static final int TF_OD_API_INPUT_SIZE = 300;
   private static final boolean TF_OD_API_IS_QUANTIZED = true;
-  private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
-  private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/labelmap.txt";
+  private static final String TF_OD_API_MODEL_FILE = "converted_yolov3_model.tflite";
+  private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/coco.names";
   private static final DetectorMode MODE = DetectorMode.TF_OD_API;
   // Minimum detection confidence to track a detection.
   private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
